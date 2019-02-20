3ad120c82db218cd3a8950b15dda9cb6178608cd
xiezom <yoshida.h3939@gmail.com>
2019-02-20 12:32:11 +0900

    2019/2/20

---
+++ TestDB/src/CheckLoadJDBC.java
@@ -0,0 +1,17 @@
+
+public class CheckloadJDBC {
+
+	public static void main(String[] args)throws 
+	InstantiationException,IllegalAccessException {
+		// TODO 自動生成されたメソッド・スタブ
+		String msg = "";
+		
+		try{
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		msg = "ドライバーのロードに成功しました";
+		} catch (ClassNotFoundException e){
+		msg = "ドライバーのロードに失敗しました";
+		}
+		System.out.println(msg);
+	}
+}
